e0323caae23f9954e1b34e92432571d1592352c6
helix-77 <mdatikmouhtasimrahi@gmail.com>
2024-08-01 05:02:04 +0600

    minimal repro build for ui bug

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { View } from "react-native";
+import React from "react";
+import { Text } from "~/components/ui/text";
+import { Button } from "~/components/ui/button";
+
+const Index = () => {
+  return (
+    <>
+      <View>
+        <Button>
+          <Text>Button Name</Text>
+        </Button>
+      </View>
+    </>
+  );
+};
+
+export default Index;
